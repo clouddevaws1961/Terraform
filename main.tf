69e56c9631267f4a0d0ee7a87d36427768b52340
clouddevaws1961 <160505014+clouddevaws1961@users.noreply.github.com>
2024-03-17 15:56:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
-provider "aws" {
-  region = "ap-south-1"
-  skip_metadata_api_check = trie
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+      region="ap-south-1"
+      skip_metadata_api_check=true
+    }
+  }
 }
 module "networking" {
   source            = "./Networking"
